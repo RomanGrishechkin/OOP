9cf07a7203d577f7a4c25b0b4aab49ca7823a1ee
Grishechkin <Grishechkin@NN-NB019>
2021-08-14 15:32:12 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _3DFigures
+{
+    class Figaro
+    {
+        private int[] H = { 0, 0, 7 };
+        private double R = 5;
+        private int N = 6;
+        private double[,] pi;       
+        public Figaro()
+        {
+            pi = new double[6, 3];
+        }
+        public Figaro(int h, double r, int n)
+        {
+            R = r;
+            N = n;
+            pi = new double[n, 3];
+            H[2] = h;
+        }
+        public void Pi()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                pi[i, 0] = R * Math.Cos(2 * Math.PI * i / (N - 1));
+                pi[i, 1] = R * Math.Sin(2 * Math.PI * i / (N - 1));
+                pi[i, 2] = 0;
+            }
+        }
+        public void PrintTriangle()
+        {
+            for (int i = N - 1; i >= 0; i--)            {
+                
+                Console.WriteLine("{3}#:\n{0}\t{1}\t{2}", pi[i, 0], pi[i, 1], H[2], i + 1);
+            }
+        }
+        public void Normal()
+        {
+            
+            double vx1, vy1, vz1, vx2, vy2, vz2;
+            
+
+            for (int i = 0; i < N; i++)
+            {
+                vx1 = H[0] - pi[i, 0];
+                vy1 = H[1] - pi[i, 1];
+                vz1 = H[2] - pi[i, 2];
+                if (i ==N - 1)
+                {
+                    vx2 = pi[i, 0] - pi[0, 0];
+                    vy2 = pi[i, 1] - pi[0, 1];
+                    vz2 = pi[i, 2] - pi[0, 2];
+                }
+                else
+                {
+                    vx2 = pi[i, 0] - pi[i + 1, 0];
+                    vy2 = pi[i, 1] - pi[i + 1, 1];
+                    vz2 = pi[i, 2] - pi[i + 1, 2];
+                }
+                Console.WriteLine("Pi{0}#Normal\n{1}\t{2}\t{3}",i+1,vy1*vz2- vz1 * vy2, vz1 * vx2 - vx1 * vz2, vx1 * vy2 - vy1 * vx2);
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _3DFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figaro conus = new Figaro();
+            conus.Pi();
+            conus.PrintTriangle();
+            conus.Normal();
+            Figaro conus1 = new Figaro(15, 10, 8);
+            conus1.Pi();
+            conus1.PrintTriangle();
+        }
+    }
+}
